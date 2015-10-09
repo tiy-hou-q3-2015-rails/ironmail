852b7b5e3389e50a123270e9222d85b098fff156
Jesse Wolgamott <jesse@comalproductions.com>
2015-10-09 14:19:49 -0500

    Initial data model for the user

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+User.create! first_name: "Jesse", last_name: "Wolgamott", email: "jesse@example.com", password: "12345678"
+User.create! first_name: "Amanda", last_name: "Wolgamott", email: "amanda@example.com", password: "12345678"
+User.create! first_name: "Jack", last_name: "Wolgamott", email: "jack@example.com", password: "12345678"
+User.create! first_name: "Hank", last_name: "Wolgamott", email: "hank@example.com", password: "12345678"
