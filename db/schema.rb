2031456466d14e2ade246747a4a0623211fcc204
Jesse Wolgamott <jesse@comalproductions.com>
2015-10-09 14:24:13 -0500

    Messages data model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009191607) do
+ActiveRecord::Schema.define(version: 20151009192020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "subject",                    null: false
+    t.text     "body",                       null: false
+    t.integer  "user_id"
+    t.string   "from_email",                 null: false
+    t.boolean  "read",       default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
@@ -25,4 +37,5 @@ ActiveRecord::Schema.define(version: 20151009191607) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "messages", "users"
 end
